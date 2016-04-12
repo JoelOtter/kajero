d0edc0105d3b3dfbccc05528893551e8265b170d
Joel Auterson <joel.auterson@googlemail.com>
2016-04-13 00:46:35 +0100

    Make object viewer collapsible

--- js/components/visualiser/ObjectVisualiser.js
+++ js/components/visualiser/ObjectVisualiser.js
@@ -3,11 +3,21 @@ import { getSpacing, selectComponent } from './Visualiser';
 
 export default class ObjectVisualiser extends Component {
 
+    constructor(props) {
+        super(props);
+        this.collapse = this.collapse.bind(this);
+        this.state = {open: false};
+    }
+
+    collapse() {
+        this.setState({open: !this.state.open});
+    }
+
     render() {
         const { data, name, indent, useHljs } = this.props;
         const keys = Object.getOwnPropertyNames(data);
         let items = [];
-        for (let i = 0; i < keys.length; i++) {
+        for (let i = 0; this.state.open && i < keys.length; i++) {
             var item = data[keys[i]];
             var VisualiserComponent = selectComponent(item);
             items.push(
@@ -22,19 +32,19 @@ export default class ObjectVisualiser extends Component {
         }
         let arrow;
         let spaces = getSpacing(indent);
-        if (items.length > 0) {
-            arrow = '\u25bc\u00a0';
+        if (keys.length > 0) {
+            arrow = this.state.open ? '\u25bc' : '\u25b6';
             if (spaces.length >= 2) {
                 // Space for arrow
                 spaces = spaces.slice(2);
             }
         }
-        const key = name ? name + ':\u00a0' : '';
+        const key = '\u00a0' + (name ?  name + ':\u00a0' : '');
 
         return (
             <div className="object-visualiser">
                 <span className="visualiser-spacing">{spaces}</span>
-                <span>{arrow}</span>
+                <span className="visualiser-arrow" onClick={this.collapse}>{arrow}</span>
                 <span>{key}</span>
                 <span className={useHljs ? "hljs-keyword" : ""}>Object</span>
                 <span>{'{}'}</span>
