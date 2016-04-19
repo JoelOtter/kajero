2da62d9507fe6d83b8f7e0e53ecf98c0229b5e4c
Joel Auterson <joel.auterson@googlemail.com>
2016-04-19 20:22:34 +0100

    Separate reducers

--- js/reducers/notebookReducer.js
+++ js/reducers/notebookReducer.js
@@ -1,12 +1,7 @@
-import { combineReducers } from 'redux';
 import Immutable from 'immutable';
-import parse from './parseMarkdown';
-import Smolder from 'smolder';
-import Jutsu from 'jutsu'; // Imports d3 and nv as globals
+import parse from '../parseMarkdown';
 import {
     LOAD_MARKDOWN,
-    RECEIVED_DATA,
-    EXECUTE,
     TOGGLE_EDIT,
     UPDATE_BLOCK,
     UPDATE_META,
@@ -17,63 +12,7 @@ import {
     MOVE_BLOCK_DOWN,
     DELETE_DATASOURCE,
     UPDATE_DATASOURCE
-} from './actions';
-
-
-/*
- * This reducer handles the state of execution of code blocks -
- * retaining results, carrying context around, and making note
- * of which blocks have and haven't been executed. It's also
- * where the obtained data is stored.
- */
-const defaultExecutionState = Immutable.Map({
-    executionContext: {},
-    data: {},
-    results: Immutable.Map(),
-    blocksExecuted: Immutable.Set()
-});
-
-function execution(state = defaultExecutionState, action) {
-    const { id, code, text, name, data } = action;
-    const stateData = state.get('data');
-    switch (action.type) {
-        case EXECUTE:
-            try {
-                let context = state.get('executionContext');
-                const data = state.get('data');
-                const newState = state.set(
-                    'blocksExecuted', state.get('blocksExecuted').add(id)
-                );
-                return newState
-                    .setIn(['results', id], executeCode(code, context, data, id))
-                    .set('executionContext', context);
-            } catch (err) {
-                console.error(err);
-                return newState
-                    .setIn(['results', id], err);
-            }
-        case RECEIVED_DATA:
-            stateData[name] = data;
-            return state.set('data', stateData);
-        case UPDATE_BLOCK:
-            return state
-                .set('blocksExecuted', state.get('blocksExecuted').remove(id))
-                .removeIn(['results', id]);
-        case DELETE_DATASOURCE:
-            delete stateData[id];
-            return state.set('data', stateData);
-        default:
-            return state;
-    }
-}
-
-function executeCode(code, context, data, id) {
-    var graphElement = document.getElementById("kajero-graph-" + id);
-    var jutsu = Smolder(Jutsu(graphElement));
-    return new Function(['d3', 'nv', 'graphs', 'data', 'graphElement'], code).call(
-        context, d3, nv, jutsu, data, graphElement
-    );
-}
+} from '../actions';
 
 /*
  * This reducer handles the state of the notebook's actual content,
@@ -88,7 +27,7 @@ const defaultNotebook = Immutable.Map({
     blocks: Immutable.Map()
 });
 
-function notebook(state = defaultNotebook, action) {
+export default function notebook(state = defaultNotebook, action) {
     const { id, text, field, blockType } = action;
     const content = state.get('content');
     switch (action.type) {
@@ -155,27 +94,3 @@ function moveItem(content, id, up) {
         .push(content.get(index + 1)).push(id)
         .concat(content.slice(index + 2));
 }
-
-/*
- * This reducer simply keeps track of the state of the editor.
- */
-const defaultEditor = Immutable.Map({
-    editable: false
-});
-
-function editor(state = defaultEditor, action) {
-    switch (action.type) {
-        case TOGGLE_EDIT:
-            return state.set('editable', !state.get('editable'));
-        default:
-            return state;
-    }
-}
-
-const reducer = combineReducers({
-    notebook,
-    execution,
-    editor
-});
-
-export default reducer;
