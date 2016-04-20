51530d7cd19e809feb1fd14a360efd7e2a5b2032
Joel Auterson <joel.auterson@googlemail.com>
2016-04-20 01:48:41 +0100

    Test editor reducer

---
+++ js/reducers/editorReducer-spec.js
@@ -0,0 +1,32 @@
+import { expect } from 'chai';
+import Immutable from 'immutable';
+import * as actions from '../actions';
+import reducer from './editorReducer';
+
+describe('editor reducer', () => {
+
+    it('should do nothing for an unhandled action type', () => {
+        expect(reducer(Immutable.Map({editable: false}), {type: 'FAKE_ACTION'}))
+            .to.eql(Immutable.Map({editable: false}));
+    });
+
+    it('should toggle editor state for TOGGLE_EDIT', () => {
+        expect(
+            reducer(
+                Immutable.Map({editable: false}),
+                {type: actions.TOGGLE_EDIT}
+            ).equals(Immutable.Map({editable: true}))
+        ).to.be.true;
+        expect(
+            reducer(
+                Immutable.Map({editable: true}),
+                {type: actions.TOGGLE_EDIT}
+            ).equals(Immutable.Map({editable: false}))
+        ).to.be.true;
+    });
+
+    it('should return the inital state', () => {
+       expect(reducer(undefined, {})).to.eql(Immutable.Map({editable: false}));
+    });
+
+});
