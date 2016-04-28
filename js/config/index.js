975415a90927daeb7adcc56d9073a4a608cf5f04
Joel Auterson <joel.auterson@googlemail.com>
2016-04-28 23:12:38 +0100

    Add minification, production config and env
    Closes #13

--- js/config/index.js
+++ js/config/index.js
@@ -1,7 +1,8 @@
 var env = process.env.NODE_ENV || 'development';
 
 var config = {
-    development: require('./development.config')
+    development: require('./development.config'),
+    production: require('./production.config')
 };
 
 module.exports = config[env];
